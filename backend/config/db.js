d71a0cc3cc2da3aef574e43adfd32d2861b27552
Raghavendra v <raghavavmurthy@gmail.com>
2024-09-26 14:36:07 +0530

    intial commit

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { ENV_VARS } from "./envVars.js";
+
+
+export const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+      console.log("MongoDB connected:" + conn.connection.host);
+    } catch(error){
+      console.error("error connecting to MONGODB:" + error.message);
+      process.exit(1);
+       
+
+    }
+
+};
